801178cfff3a1456c7446884d17147471da47613
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-23 17:33:36 -0300

    Added ctor to AppDbContext

--- ClientAPI/Context/AppDbContext.cs
+++ ClientAPI/Context/AppDbContext.cs
@@ -5,6 +5,9 @@ namespace ClientAPI.Context
 {
     public class AppDbContext : DbContext
     {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+
         DbSet<Client>? Clients { get; set; }
     }
 }
