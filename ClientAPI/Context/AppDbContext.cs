be8ac1354cf5163d06ed6cc435a1d84fe14afab1
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-23 17:13:51 -0300

    Added DBContext

---
+++ ClientAPI/Context/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ClientAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Client>? Clients { get; set; }
+    }
+}
