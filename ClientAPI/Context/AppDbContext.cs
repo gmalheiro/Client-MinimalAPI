870ec1d54bfb759967cb58584cddeec65fc0bb6b
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-23 20:08:57 -0300

    Turned Clients prop public

--- ClientAPI/Context/AppDbContext.cs
+++ ClientAPI/Context/AppDbContext.cs
@@ -8,6 +8,6 @@ namespace ClientAPI.Context
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
 
-        DbSet<Client>? Clients { get; set; }
+        public DbSet<Client>? Clients { get; set; }
     }
 }
