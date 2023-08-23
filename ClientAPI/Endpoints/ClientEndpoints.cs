a5584c71967eae3bc032e6105a1d64ad52037ba4
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-23 20:17:55 -0300

    Get all method done

---
+++ ClientAPI/Endpoints/ClientEndpoints.cs
@@ -0,0 +1,22 @@
+﻿using ClientAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientAPI.Endpoints
+{
+    public static class ClientEndpoints
+    {
+        public static void MapTarefasEndpoints(this WebApplication app)
+        {
+            app.MapGet("/GetAllClients", async (AppDbContext db) =>
+            {
+                var clients =  await db?.Clients?.ToListAsync()!;
+
+                if (clients is null)
+                    return Results.NotFound("Clients not found");
+
+                return Results.Ok(clients);
+
+            } );
+        }
+    }
+}
